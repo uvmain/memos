d863837c0714472495a498cf6c9a2c4254ce4274
Ian Baron <uv.fragm@gmail.com>
2024-07-22 18:33:35 +0100

    enable autocomplete for login

--- src/App.vue
+++ src/App.vue
@@ -59,15 +59,27 @@ watch(notes, (newNotes) => {
     <div class="sm:col-span-2 sm:col-start-1 mt-4">
       <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
       <div class="mt-2">
-        <input v-model="username" type="text" name="username" id="username" autocomplete="none"
+        <input
+          v-model="username"
+          type="text"
+          name="username"
+          id="username"
+          autocomplete="email"
           placeholder="Enter Email Address "
           class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
       </div>
     </div>
     <div class="sm:col-span-2 sm:col-start-1 mt-2">
-      <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+      <label for="password" class="block text-sm font-medium leading-6 text-gray-900">
+        Password
+      </label>
       <div class="mt-2">
-        <input v-model="password" type="password" name="password" id="password" autocomplete="none"
+        <input
+          v-model="password"
+          type="password"
+          name="password"
+          id="password"
+          autocomplete="password"
           placeholder="Enter Password "
           class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
       </div>
